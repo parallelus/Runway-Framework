d8bb2abc7165105e98dbfe4d87ae358ae6837154
JeffMatson <jeff@jeffmatson.net>
2014-07-19 01:48:01 -0400

    Update settings-object.php

--- runway-framework/framework/includes/theme-updater/settings-object.php
+++ runway-framework/framework/includes/theme-updater/settings-object.php
@@ -117,7 +117,7 @@ class Theme_Updater_Admin_Object extends Runway_Admin_Object {
 		}
 
 		if($item == 'theme') {
-			$info = file_get_contents( ABSPATH . 'wp-content/themes/runway-framework/style.css' );
+			$info = file_get_contents( ABSPATH . 'get_template_directory() . style.css' );
 			$start = strpos( $info, 'Github Theme URI' );
 			$gtu = '';
 			if($start > 0) {
