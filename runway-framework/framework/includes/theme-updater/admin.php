717c2ae4b9a2188dad7568db4b88b19316780a77
Andy <andy@parallelus.net>
2014-08-05 11:52:04 -0500

    Update to issue #61 for improved translation strings.

--- runway-framework/framework/includes/theme-updater/admin.php
+++ runway-framework/framework/includes/theme-updater/admin.php
@@ -7,10 +7,10 @@ global $theme_updater_admin, $auth_manager_admin;
 
 <?php
 	if(!isset($auth_manager_admin->token)) {
-		echo __('Please sign in with ', 'framework'); ?><a href="<?php echo network_admin_url('admin.php?page=accounts'); ?>"><?php echo __('Accounts', 'framework'); ?></a><?php 
+		printf( __('Please sign in to your %sParallelus Account%s.', 'framework'), '<a href="'. network_admin_url("admin.php?page=accounts") .'">', '</a>' );
 	}
 	else {
-		echo __('You can get updates in ', 'framework'); ?><a href="<?php echo network_admin_url('update-core.php'); ?>"><?php echo __('Update core', 'framework'); ?></a><?php 
+		printf( __('You can check for new versions from the %sUpdates%s area of your admin.', 'framework'), '<a href="'. network_admin_url("update-core.php") .'">', '</a>' );
 	}
 ?>
 
