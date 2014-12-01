db3fd560aad8d5bda5d9e1e3d39fab11898811ca
Alexx G <alexxgermann@yandex.ru>
2014-12-01 16:24:19 +0200

    Fixed issue #96 - Errors installing extensions

---
+++ runway-framework/framework/includes/download-directory/views/error-msg.php
@@ -0,0 +1,8 @@
+<?php
+
+$ext_err_name = (isset($this->extensions_addons->$item->Name) && !empty($this->extensions_addons->$item->Name))? $this->extensions_addons->$item->Name : __('Unknown', 'framework');
+$error_msg = (isset($body['error_message']) && !empty($body['error_message']))? rf__('There was an error while attempting to install the '.$ext_err_name.' extension: ') . $body['error_message'] :
+																				rf__('There was an error while attempting to install the '.$ext_err_name.' extension');
+echo '<div id="message" class="error"><p>' . $error_msg . '</p></div>';
+
+?>
