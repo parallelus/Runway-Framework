e12d769a878b6aff4e99e300568e1cbc0c36ae04
Alexx G <alexxgermann@yandex.ru>
2016-05-26 15:57:47 +0300

    Translation strings updates for variables. Fixes to issue #166

--- runway-framework/framework/includes/download-directory/views/error-msg.php
+++ runway-framework/framework/includes/download-directory/views/error-msg.php
@@ -1,5 +1,5 @@
 <?php
 
 $ext_err_name = (isset($this->extensions_addons->$item->Name) && !empty($this->extensions_addons->$item->Name))? $this->extensions_addons->$item->Name : __('Unknown', 'runway');
-$error_msg = (isset($body['error_message']) && !empty($body['error_message']))? rf__('There was an error while attempting to install the '.$ext_err_name.' extension: ') . $body['error_message'] : rf__('There was an error while attempting to install the '.$ext_err_name.' extension');
+$error_msg = (isset($body['error_message']) && !empty($body['error_message']))? sprintf( __('There was an error while attempting to install the %s  extension: %s', 'runway'), $ext_err_name, $body['error_message'] ) : sprintf( __('There was an error while attempting to install the %s extension', 'runway'), $ext_err_name );
 echo '<div id="message" class="error"><p>' . $error_msg . '</p></div>';
