66d2b039bb4de11feca33f113e170496a5bc0a3e
Alexx G <alexxgermann@yandex.ru>
2016-10-07 15:40:32 +0300

    Refactoring for Dashboard

--- runway-framework/framework/includes/dashboard/views/introduction.php
+++ runway-framework/framework/includes/dashboard/views/introduction.php
@@ -2,55 +2,85 @@
 
 	<div class="intro-panel-content">
 
-		<!-- <h3>Introduction to Runway</h3> -->
-		<!-- <p class="about-description">
-			Jump in quickly by exploring Runway's features listed below. You can find more details on using Runway in the additional information from the sections that follow.
-		</p> -->
-
 		<div class="intro-panel-column-container">
 			<div class="intro-panel-column">
-				<h4><span class="icon16 icon-appearance"></span> <?php echo __('Build Themes', 'runway'); ?></h4>
-				<p><?php echo __('Making themes is what Runway is about. Create new child themes, custom designs or download a Runway child theme.', 'runway'); ?></p>
+				<h4><span class="icon16 icon-appearance"></span> <?php echo __( 'Build Themes', 'runway' ); ?></h4>
+				<p>
+					<?php echo __( 'Making themes is what Runway is about. Create new child themes, custom designs or download a Runway child theme.', 'runway' ); ?>
+				</p>
 				<ul class="bullet-list">
-					<li><a href="<?php echo admin_url('admin.php?page=themes&amp;navigation=new-theme'); ?>"><?php echo __('Create a new Runway child theme', 'runway'); ?></a></li>
-					<li><a href="<?php echo admin_url('admin.php?page=themes'); ?>"><?php echo __('Activate an existing Runway theme', 'runway'); ?></a></li>
-					<li><a href="<?php echo admin_url('admin.php?page=directory'); ?>"><?php echo __('Search for more themes', 'runway'); ?></a></li>
-					<!-- <li><a href="#">Activate the default Runway child theme</a></li> -->
+					<li>
+						<a href="<?php echo admin_url( 'admin.php?page=themes&amp;navigation=new-theme' ); ?>">
+							<?php echo __( 'Create a new Runway child theme', 'runway' ); ?>
+						</a>
+					</li>
+					<li>
+						<a href="<?php echo admin_url( 'admin.php?page=themes' ); ?>">
+							<?php echo __( 'Activate an existing Runway theme', 'runway' ); ?>
+						</a>
+					</li>
+					<li>
+						<a href="<?php echo admin_url( 'admin.php?page=directory' ); ?>">
+							<?php echo __( 'Search for more themes', 'runway' ); ?>
+						</a>
+					</li>
 				</ul>
 			</div>
+
 			<div class="intro-panel-column">
-				<h4><span class="icon16 icon-settings"></span> <?php echo __('Extend and Enhance', 'runway'); ?></h4>
-				<p><?php echo __('Extensions quickly add features to your theme, like a plugin integrated with the theme for easy distribution.', 'runway'); ?></p>
+				<h4><span class="icon16 icon-settings"></span> <?php echo __( 'Extend and Enhance', 'runway' ); ?></h4>
+				<p>
+					<?php echo __( 'Extensions quickly add features to your theme, like a plugin integrated with the theme for easy distribution.', 'runway' ); ?>
+				</p>
 				<ul class="bullet-list">
-					<li><a href="<?php echo admin_url('admin.php?page=extensions'); ?>"><?php echo __('Activate Extensions', 'runway'); ?></a></li>
-					<li><a href="<?php echo admin_url('admin.php?page=directory'); ?>"><?php echo __('Browse the directory', 'runway'); ?></a></li>
-					<li><a href="<?php echo admin_url('admin.php?page=directory&amp;tab=search'); ?>"><?php echo __('Search all Extensions', 'runway'); ?></a></li>
+					<li>
+						<a href="<?php echo admin_url( 'admin.php?page=extensions' ); ?>">
+							<?php echo __( 'Activate Extensions', 'runway' ); ?>
+						</a>
+					</li>
+					<li>
+						<a href="<?php echo admin_url( 'admin.php?page=directory' ); ?>">
+							<?php echo __( 'Browse the directory', 'runway' ); ?>
+						</a>
+					</li>
+					<li>
+						<a href="<?php echo admin_url( 'admin.php?page=directory&amp;tab=search' ); ?>">
+							<?php echo __( 'Search all Extensions', 'runway' ); ?>
+						</a>
+					</li>
 				</ul>
 			</div>
+
 			<div class="intro-panel-column intro-panel-last">
-				<h4><span class="icon16 icon-page"></span> <?php echo __('Customize the Admin', 'runway'); ?></h4>
-				<p><?php echo __('Create options pages, admin menus and much more. The Options Builder interface allows you to create theme options.', 'runway'); ?></p>
+				<h4><span class="icon16 icon-page"></span> <?php echo __( 'Customize the Admin', 'runway' ); ?></h4>
+				<p>
+					<?php echo __( 'Create options pages, admin menus and much more. The Options Builder interface allows you to create theme options.', 'runway' ); ?>
+				</p>
 				<ul class="bullet-list">
-					<li><a href="<?php echo admin_url('admin.php?page=options-builder'); ?>"><?php echo __('Add theme options pages', 'runway'); ?></a></li>
+					<li>
+						<a href="<?php echo admin_url( 'admin.php?page=options-builder' ); ?>">
+							<?php echo __( 'Add theme options pages', 'runway' ); ?>
+						</a>
+					</li>
 
 					<?php
 					global $extm;
 
 					$admin_menu_activated = $extm->is_activated('admin-menu-editor/load.php');
 					if ( $admin_menu_activated === true ) {
-						?>
-
-						<li>
-							<a href="<?php echo admin_url( 'admin.php?page=admin-menu' ); ?>">
-								<?php echo __( 'Manage admin menus', 'runway' ); ?>
-							</a>
-						</li>
+					?>
+					
+					<li>
+						<a href="<?php echo admin_url( 'admin.php?page=admin-menu' ); ?>">
+							<?php echo __( 'Manage admin menus', 'runway' ); ?>
+						</a>
+					</li>
 
 					<?php } ?>
-
 				</ul>
 			</div>
 		</div>
 
 	</div>
+
 </div>
--- runway-framework/framework/includes/dashboard/views/release-notes.php
+++ runway-framework/framework/includes/dashboard/views/release-notes.php
@@ -1,43 +1,42 @@
-
-	<div class="changelog">
-		<ul class="release-list">
-			<li>
-				<h3><?php echo __('Release', 'runway'); ?></h3>
-				<p>
-					<strong><?php echo __('Version 1.2', 'runway'); ?></strong>
-				</p>
-			</li>
-			<li>
-				<h3><?php echo __('Release', 'runway'); ?></h3>
-				<p>
-					<strong><?php echo __('Version 1.1.1', 'runway'); ?></strong>
-				</p>
-			</li>
-			<li>
-				<h3><?php echo __('Release', 'runway'); ?></h3>
-				<p>
-					<strong><?php echo __('Version 1.1', 'runway'); ?></strong>
-				</p>
-			</li>
-			<li>
-				<h3><?php echo __('Release', 'runway'); ?></h3>
-				<p>
-					<strong><?php echo __('Version 1.0.1', 'runway'); ?></strong>
-				</p>
-			</li>
-			<li>
-				<h3><?php echo __('Initial Release', 'runway'); ?></h3>
-				<p>
-					<strong><?php echo __('Version 1.0', 'runway'); ?></strong>
-					- <?php echo __('Initial release version.', 'runway'); ?>.
-				</p>
-			</li>
-			<li>
-				<h3><?php echo __('Beta 1', 'runway'); ?></h3>
-				<p>
-					<strong><?php echo __('Version 0.1', 'runway'); ?></strong>
-					- <?php echo __('Core functionality created', 'runway'); ?>.
-				</p>
-			</li>
-		</ul>
-	</div>
+<div class="changelog">
+	<ul class="release-list">
+		<li>
+			<h3><?php echo __( 'Release', 'runway' ); ?></h3>
+			<p>
+				<strong><?php echo __( 'Version 1.2', 'runway' ); ?></strong>
+			</p>
+		</li>
+		<li>
+			<h3><?php echo __( 'Release', 'runway' ); ?></h3>
+			<p>
+				<strong><?php echo __( 'Version 1.1.1', 'runway' ); ?></strong>
+			</p>
+		</li>
+		<li>
+			<h3><?php echo __( 'Release', 'runway' ); ?></h3>
+			<p>
+				<strong><?php echo __( 'Version 1.1', 'runway' ); ?></strong>
+			</p>
+		</li>
+		<li>
+			<h3><?php echo __( 'Release', 'runway' ); ?></h3>
+			<p>
+				<strong><?php echo __( 'Version 1.0.1', 'runway' ); ?></strong>
+			</p>
+		</li>
+		<li>
+			<h3><?php echo __( 'Initial Release', 'runway' ); ?></h3>
+			<p>
+				<strong><?php echo __( 'Version 1.0', 'runway' ); ?></strong>
+				- <?php echo __( 'Initial release version.', 'runway' ); ?>.
+			</p>
+		</li>
+		<li>
+			<h3><?php echo __( 'Beta 1', 'runway' ); ?></h3>
+			<p>
+				<strong><?php echo __( 'Version 0.1', 'runway' ); ?></strong>
+				- <?php echo __( 'Core functionality created', 'runway' ); ?>.
+			</p>
+		</li>
+	</ul>
+</div>
