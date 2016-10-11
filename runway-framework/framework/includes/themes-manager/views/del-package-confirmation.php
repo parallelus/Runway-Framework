cdff3fe1efbcc51497e25c96060615c30df83de5
Alexx G <alexxgermann@yandex.ru>
2016-10-11 15:34:38 +0300

    Refactoring for themes-manager (views)

--- runway-framework/framework/includes/themes-manager/views/del-package-confirmation.php
+++ runway-framework/framework/includes/themes-manager/views/del-package-confirmation.php
@@ -1,10 +1,16 @@
-<p><?php echo __('You are about to remove the following packages from server', 'runway'); ?>:</p>
+<p><?php echo __( 'You are about to remove the following packages from server', 'runway' ); ?>:</p>
 
 <ul class="ul-disc">
-    <li><?php echo __('Standalone package from', 'runway'); ?>: <strong><?php echo wp_kses_post($package_info['date'].' '.$package_info['time'].' ('.$package_info['a_file'].')'); ?></strong></li>
-    <li><?php echo __('Child package from', 'runway'); ?>: <strong><?php echo wp_kses_post($package_info['date'].' '.$package_info['time'].' ('.$package_info['c_file'].')'); ?></strong></li>
+	<li><?php echo __( 'Standalone package from', 'runway' ); ?>:
+		<strong><?php echo wp_kses_post( $package_info['date'] . ' ' . $package_info['time'] . ' (' . $package_info['a_file'] . ')' ); ?></strong>
+	</li>
+	<li><?php echo __( 'Child package from', 'runway' ); ?>:
+		<strong><?php echo wp_kses_post( $package_info['date'] . ' ' . $package_info['time'] . ' (' . $package_info['c_file'] . ')' ); ?></strong>
+	</li>
 </ul>
 
-<p><?php _e('Are you sure you wish to delete these files?', 'runway'); ?></p>
-<a href="<?php echo esc_url( $developer_tools->self_url('do-package').'&name='.$name.'&action=delete-package&package='.$package ); ?>" class = "button-secondary"><?php echo __('Yes, Delete these files', 'runway'); ?></a>
-<a href="<?php echo esc_url( $developer_tools->self_url('do-package').'&name='.$name ); ?>" class = "button-secondary"><?php echo __('No, Return me to the theme list', 'runway'); ?></a>
+<p><?php _e( 'Are you sure you wish to delete these files?', 'runway' ); ?></p>
+<a href="<?php echo esc_url( $developer_tools->self_url( 'do-package' ) . '&name=' . $name . '&action=delete-package&package=' . $package ); ?>"
+   class="button-secondary"><?php echo __( 'Yes, Delete these files', 'runway' ); ?></a>
+<a href="<?php echo esc_url( $developer_tools->self_url( 'do-package' ) . '&name=' . $name ); ?>"
+   class="button-secondary"><?php echo __( 'No, Return me to the theme list', 'runway' ); ?></a>
--- runway-framework/framework/includes/themes-manager/views/del-theme-confirmation.php
+++ runway-framework/framework/includes/themes-manager/views/del-theme-confirmation.php
@@ -1,9 +1,13 @@
-<p><?php echo __('You are about to remove the following theme', 'runway'); ?>:</p>
+<p><?php echo __( 'You are about to remove the following theme', 'runway' ); ?>:</p>
 
 <ul class="ul-disc">
-    <li><strong><?php echo wp_kses_post($del_theme_info['Name']); ?></strong> <?php echo __('by', 'runway'); ?> <em><?php echo ( $del_theme_info['Author'] != '' ) ? wp_kses_post($del_theme_info['Author']) : 'Runway Framework'; ?></em></li>
+	<li><strong><?php echo wp_kses_post( $del_theme_info['Name'] ); ?></strong> <?php echo __( 'by', 'runway' ); ?>
+		<em><?php echo ( $del_theme_info['Author'] != '' ) ? wp_kses_post( $del_theme_info['Author'] ) : 'Runway Framework'; ?></em>
+	</li>
 </ul>
 
-<p><?php echo __('Are you sure you wish to delete these files', 'runway'); ?>?</p>
-<a href="<?php echo esc_url( admin_url('admin.php?page=themes&navigation=delete-theme&name='.$del_theme_info['Folder'].'&confirm=true') ); ?>" class = "button-secondary"><?php echo __('Yes, Delete these files', 'runway'); ?></a>
-<a href="<?php echo esc_url( admin_url('admin.php?page=themes') ); ?>" class = "button-secondary"><?php echo __('No, Return me to the theme list', 'runway'); ?></a>
+<p><?php echo __( 'Are you sure you wish to delete these files', 'runway' ); ?>?</p>
+<a href="<?php echo esc_url( admin_url( 'admin.php?page=themes&navigation=delete-theme&name=' . $del_theme_info['Folder'] . '&confirm=true' ) ); ?>"
+   class="button-secondary"><?php echo __( 'Yes, Delete these files', 'runway' ); ?></a>
+<a href="<?php echo esc_url( admin_url( 'admin.php?page=themes' ) ); ?>"
+   class="button-secondary"><?php echo __( 'No, Return me to the theme list', 'runway' ); ?></a>
