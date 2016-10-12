b5529d0dc805f7e8f120434794e8d62343a6df6b
Alexx G <alexxgermann@yandex.ru>
2016-10-12 14:33:51 +0300

    Refactoring for options-builder (admin.php, load.php, settings-object.php)

--- runway-framework/framework/includes/options-builder/admin.php
+++ runway-framework/framework/includes/options-builder/admin.php
@@ -3,143 +3,158 @@ global $apm, $alias_;
 
 $pages_dir = $apm->pages_dir;
 
-if ( get_stylesheet_directory() == get_template_directory() ) {
-	echo '<br>'. __('You must create or activate a Runway child theme to add options pages', 'runway').': <a href="'.admin_url('admin.php?page=themes').'">'.__('Runway Themes', 'runway').'</a>';
+if ( ! function_exists( 'get_copy_alias' ) ) {
+	function get_copy_alias( $alias ) {
+
+		global $apm, $alias_;
+		$pages     = $apm->get_pages_list();
+		$check_sum = 0;
+		foreach ( $pages as $page ) {
+			if ( $alias == $page->settings->alias ) {
+				$check_sum++;
+			}
+		}
+
+		if ( $check_sum > 0 ) {
+			$alias_ .= '-copy';
+			get_copy_alias( $alias . '-copy' );
+		}
+
+	}
 }
-else {
-	if ( !isset( $this->navigation ) || empty( $this->navigation ) )
+
+if ( get_stylesheet_directory() == get_template_directory() ) {
+	echo '<br>' . __( 'You must create or activate a Runway child theme to add options pages', 'runway' ) .
+	     ': <a href="' . admin_url( 'admin.php?page=themes' ) . '">' . __( 'Runway Themes', 'runway' ) . '</a>';
+} else {
+	if ( ! isset( $this->navigation ) || empty( $this->navigation ) ) {
 		$this->navigation = 'list-pages';
+	}
 
 	switch ( $this->navigation ) {
-	case 'new-page': {
+		case 'new-page': {
 			$new_page_id = time();
 
 			$page = array(
 				'settings' => array(
-					'page_id' => $new_page_id,
-					'title' => __('New Options Page', 'runway'),
-					'alias' => 'options-page',
+					'page_id'          => $new_page_id,
+					'title'            => __( 'New Options Page', 'runway' ),
+					'alias'            => 'options-page',
 					'adminMenuTopItem' => 'current-theme',
-					'showPageTitle' => 'true',
+					'showPageTitle'    => 'true',
 				),
 				'elements' => array(),
 			);
 
 			$page_json = json_encode( $page );
 
-			include_once 'views/page-builder.php';
+			include_once __DIR__ . '/views/page-builder.php';
 
-		} break;
+		}
+			break;
 
 		// edit page
-	case 'edit-page': {
+		case 'edit-page': {
 			$wp_filesystem = get_runway_wp_filesystem();
 
 			$page_id = $_GET['page_id'];
 
-			if ( file_exists( $pages_dir.$page_id.'.json' ) ) {
+			if ( file_exists( $pages_dir . $page_id . '.json' ) ) {
 				$page_json = $wp_filesystem->get_contents( runway_prepare_path( $pages_dir . $page_id . '.json' ) );
-				$page = json_decode( $page_json, true );
+				$page      = json_decode( $page_json, true );
 
-				$page = $apm->inputs_decode( $page );
+				$page      = $apm->inputs_decode( $page );
 				$page_json = addslashes( $page_json );
 
-				include_once 'views/page-builder.php';
+				include_once __DIR__ . '/views/page-builder.php';
 			} else {
-				wp_die( __('Page not found', 'runway') );
+				wp_die( __( 'Page not found', 'runway' ) );
 			}
-		} break;
+		}
+			break;
+
 		// list available pages
-	case 'list-pages': {
+		case 'list-pages': {
 			$pages = $apm->get_pages_list();
-			include_once 'views/list-pages.php';
+			include_once __DIR__ . '/views/list-pages.php';
 
-		} break;
+		}
+			break;
 
-	case 'confirm-remove-page':{
+		case 'confirm-remove-page': {
 			$wp_filesystem = get_runway_wp_filesystem();
-			$page_id = $_GET['page_id'];
+			$page_id       = $_GET['page_id'];
 
-			if ( file_exists( $pages_dir.$page_id.'.json' ) ) {
+			if ( file_exists( $pages_dir . $page_id . '.json' ) ) {
 				$page_json = $wp_filesystem->get_contents( runway_prepare_path( $pages_dir . $page_id . '.json' ) );
-				$page = json_decode( $page_json, true );
+				$page      = json_decode( $page_json, true );
 
-				$item_confirm = 'option page';
-				$item_title = $page['settings']['title'];
-				$action_url_yes = admin_url('admin.php?page=options-builder&navigation=remove-page&page_id='.$page_id);
-				$action_url_no = admin_url('admin.php?page=options-builder');
+				$item_confirm   = 'option page';
+				$item_title     = $page['settings']['title'];
+				$action_url_yes = admin_url( 'admin.php?page=options-builder&navigation=remove-page&page_id=' . $page_id );
+				$action_url_no  = admin_url( 'admin.php?page=options-builder' );
 
-				require_once(get_template_directory().'/framework/templates/delete-confirmation.php');
+				require_once get_template_directory() . '/framework/templates/delete-confirmation.php';
 			} else {
-				wp_die( __('Page not found', 'runway') );
+				wp_die( __( 'Page not found', 'runway' ) );
 			}
-	} break;
+		}
+			break;
 
-	case 'remove-page': {
+		case 'remove-page': {
 			$apm->del_page( $_GET['page_id'], $pages_dir );
 			$pages = $apm->get_pages_list();
 
-			include_once 'views/list-pages.php';
-		} break;
+			include_once __DIR__ . '/views/list-pages.php';
+		}
+			break;
 
-	case 'duplicate-page': {
+		case 'duplicate-page': {
 			$wp_filesystem = get_runway_wp_filesystem();
 
 			$page_id = $_GET['page_id'];
 
-			if ( file_exists( $pages_dir.$page_id.'.json' ) ) {
+			if ( file_exists( $pages_dir . $page_id . '.json' ) ) {
 				$page_json = $wp_filesystem->get_contents( runway_prepare_path( $pages_dir . $page_id . '.json' ) );
-				$page = json_decode( $page_json );
+				$page      = json_decode( $page_json );
 
 				$page->settings->page_id = time();
-				$page->settings->title = $page->settings->title . ' ('.__('copy', 'runway').')';
-				$new_alias = sanitize_title( $page->settings->title );
-				$alias_ = $new_alias;
+				$page->settings->title   = $page->settings->title . ' (' . __( 'copy', 'runway' ) . ')';
+				$new_alias               = sanitize_title( $page->settings->title );
+				$alias_                  = $new_alias;
 				get_copy_alias( $new_alias );
 				$page->settings->alias = $alias_;
 
 				$page_json = json_encode( $page );
 				$wp_filesystem->put_contents(
 					runway_prepare_path( $pages_dir . $page->settings->page_id . '.json' ),
-					$page_json, 
+					$page_json,
 					FS_CHMOD_FILE
 				);
 
 				$pages = $apm->get_pages_list();
-				include_once 'views/list-pages.php';
+				include_once __DIR__ . '/views/list-pages.php';
 			} else {
-				wp_die( __('Page not found', 'runway') );
+				wp_die( __( 'Page not found', 'runway' ) );
 			}
 
-			include_once 'views/list-pages.php';
-		} break;
+			include_once __DIR__ . '/views/list-pages.php';
+		}
+			break;
 
-	case 'reset-fields-page':{
+		case 'reset-fields-page': {
 			$apm->reset_to_default( $pages_dir, $_GET['page_id'] );
 			$pages = $apm->get_pages_list();
 
-			include_once 'views/list-pages.php';
-		} break;
-
-	default : {
-			include_once 'views/list-pages.php';
-		} break;
-	}
-}
-
-function get_copy_alias( $alias ) {
-	global $apm, $alias_;
-	$pages = $apm->get_pages_list();
-	$check_sum = 0;
-	foreach ( $pages as $page ) {
-		if ( $alias == $page->settings->alias ) {
-			$check_sum++;
+			include_once __DIR__ . '/views/list-pages.php';
 		}
+			break;
+
+		default : {
+			include_once __DIR__ . '/views/list-pages.php';
+		}
+			break;
+
 	}
 
-	if ( $check_sum > 0 ) {
-		$alias_ = $alias_.'-copy';
-		get_copy_alias( $alias.'-copy' );
-	}
 }
-?>
--- runway-framework/framework/includes/options-builder/load.php
+++ runway-framework/framework/includes/options-builder/load.php
@@ -12,93 +12,105 @@
 	Site Wide Only:
 */
 
-
-function try_to_create_folder( $pathname ) {
-	if ( is_writable( $pathname ) ) {
-		mkdir( $pathname, 0755, true );
-		return true;
-	} else {
-		return false;
-	}
-}
-
 // Create pages dir
-if ( !file_exists( get_stylesheet_directory().'/data/pages' ) ) {
-	try_to_create_folder( get_stylesheet_directory().'/data/pages' );
+if ( ! file_exists( get_stylesheet_directory() . '/data/pages' ) ) {
+	try_to_create_folder( get_stylesheet_directory() . '/data/pages' );
 }
 
 // Load this section of the admin
 $fields = array(
-	'var' => array(),
+	'var'   => array(),
 	'array' => array()
 );
 
 $default = array();
 
 $settings = array(
-	'name' => __('Options Builder', 'runway'),
-	'option_key' => $shortname.'options-builder',
-	'fields' => $fields,
-	'default' => $default,
-	'parent_menu' => 'hidden', // managed by framework
+	'name'             => __( 'Options Builder', 'runway' ),
+	'option_key'       => $shortname . 'options-builder',
+	'fields'           => $fields,
+	'default'          => $default,
+	'parent_menu'      => 'hidden', // managed by framework
 	'menu_permissions' => 'administrator',
-	'file' => __FILE__,
-	'js' => array(
+	'file'             => __FILE__,
+	'js'               => array(
 		'wp-color-picker',
 		'formsbuilder',
 		'ace',
 		'rw_nouislider',
-		FRAMEWORK_URL.'framework/js/farbtastic/farbtastic.js',
+		FRAMEWORK_URL . 'framework/js/farbtastic/farbtastic.js',
 	),
-	'css' => array(
+	'css'              => array(
 		'wp-color-picker',
 		'formsbuilder-style',
 		'rw_nouislider_css',
-		FRAMEWORK_URL.'framework/js/farbtastic/farbtastic.css',
+		FRAMEWORK_URL . 'framework/js/farbtastic/farbtastic.css',
 	),
 );
 
 // Required components
-include 'object.php';
+include __DIR__ . '/object.php';
 
 global $apm;
 $ApmAdmin = new Apm_Settings_Object( $settings );
 
 // Load admin components
 if ( is_admin() ) {
-	include 'settings-object.php';
+	include __DIR__ . '/settings-object.php';
 	$apm = new Apm_Admin( $settings );
 }
 
 do_action( 'options_builder_is_load' );
 
-// Setup a custom button in the title
-function title_button_add( $title ) {
-	if ( $_GET['page'] == 'options-builder' ) {
-		$title .= ' <a href="'.admin_url('admin.php?page=options-builder&navigation=new-page').'" class="add-new-h2">'. __( 'New Admin Page', 'runway' ) .'</a>';
+if ( ! function_exists( 'options_page_render_report' ) ) {
+	// Setup a custom button in the title
+	function title_button_add( $title ) {
+
+		if ( $_GET['page'] == 'options-builder' ) {
+			$title .= ' <a href="' . admin_url( 'admin.php?page=options-builder&navigation=new-page' ) . '" class="add-new-h2">' . __( 'New Admin Page',
+					'runway' ) . '</a>';
+		}
+
+		return $title;
+
 	}
-	return $title;
+	add_filter( 'framework_admin_title', 'title_button_add' );
 }
-add_filter( 'framework_admin_title', 'title_button_add' );
 
-add_action( 'add_report', 'options_page_render_report' );
+if ( ! function_exists( 'options_page_render_report' ) ) {
+	function options_page_render_report( $reports_object ) {
 
-function options_page_render_report( $reports_object ) {
-	$pages_dir = get_stylesheet_directory() . '/data/pages/';
-	$reports_object->assign_report( array(
-			'source' => 'Options Builder',
-			'report_key' => 'pages_dir_exists',
-			'path' => $pages_dir,
-            'success_message' => sprintf( __('Pages dir (%s) is exists.', 'runway'), $pages_dir ),
-            'fail_message' => sprintf( __('Pages dir (%s) is not exists.', 'runway'), $pages_dir ),
+		$pages_dir = get_stylesheet_directory() . '/data/pages/';
+		$reports_object->assign_report( array(
+			'source'          => 'Options Builder',
+			'report_key'      => 'pages_dir_exists',
+			'path'            => $pages_dir,
+			'success_message' => sprintf( __( 'Pages dir (%s) is exists.', 'runway' ), $pages_dir ),
+			'fail_message'    => sprintf( __( 'Pages dir (%s) is not exists.', 'runway' ), $pages_dir ),
 		), 'DIR_EXISTS' );
 
-	$reports_object->assign_report( array(
-			'source' => 'Options Builder',
-			'report_key' => 'pages_dir_writable',
-			'path' => $pages_dir,
-            'success_message' => sprintf( __('Pages dir (%s) is writable.', 'runway'), $pages_dir ),
-            'fail_message' => sprintf( __('Pages dir (%s) is not writable.', 'runway'), $pages_dir ),
+		$reports_object->assign_report( array(
+			'source'          => 'Options Builder',
+			'report_key'      => 'pages_dir_writable',
+			'path'            => $pages_dir,
+			'success_message' => sprintf( __( 'Pages dir (%s) is writable.', 'runway' ), $pages_dir ),
+			'fail_message'    => sprintf( __( 'Pages dir (%s) is not writable.', 'runway' ), $pages_dir ),
 		), 'IS_WRITABLE' );
+
+	}
+	add_action( 'add_report', 'options_page_render_report' );
 }
-?>
+
+if ( ! function_exists( 'try_to_create_folder' ) ) {
+	function try_to_create_folder( $pathname ) {
+
+		if ( is_writable( $pathname ) ) {
+			mkdir( $pathname, 0755, true );
+
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+}
